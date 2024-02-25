3e63fb8b585b3d8e7bdbfcd5e809db53e535db4b
mustafaozkan <info@mustafaozkan.dev>
2024-02-25 15:54:12 +0300

    Connect to remote db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  database: 'PhrasePilot',
-  username: process.env.POSTGRE_USERNAME,
-  password: process.env.POSTGRE_PASSWORD,
-  host: 'localhost',
-  dialect: 'postgres' // Adjust the dialect if you are using a different database
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRE_DB_NAME,
+  process.env.POSTGRE_USERNAME,
+  process.env.POSTGRE_PASSWORD,
+  {
+    host: process.env.POSTGRE_HOST_IP, // Use the IP address of worker3 or 'localhost' if the code runs on the same host
+    port: process.env.POSTGRE_PORT, // The port you mapped in your Docker command
+    dialect: 'postgres',
+    pool: {
+      max: 5, // Maximum number of connection in pool
+      min: 0, // Minimum number of connection in pool
+      acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
+      idle: 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
+    }
+  }
+);
 
 module.exports = sequelize;
